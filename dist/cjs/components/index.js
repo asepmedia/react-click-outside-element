1d35a867535986713534f7feef49046cd1dec809
asepmedia <38859211+asepmedia@users.noreply.github.com>
2023-06-29 09:15:11 +0700

    feat: build for version 0.2.3

--- dist/cjs/components/index.js
+++ dist/cjs/components/index.js
@@ -8,14 +8,22 @@ function OutsideWrapper(props) {
     var children = props.children, onClickOutside = props.onClickOutside;
     // use ref for detect element
     var wrapperRef = (0, react_1.useRef)(null);
+    var _a = (0, react_1.useState)(false), isClicked = _a[0], setIsClicked = _a[1];
+    var firstClicked = function () {
+        setIsClicked(true);
+    };
+    var lastClicked = function () {
+        setIsClicked(false);
+    };
     // use hooks outside and every change compare to current ref
     (0, hooks_1.useOutsideAlerter)(wrapperRef, function (e) {
-        if (onClickOutside) {
+        if (onClickOutside && isClicked) {
+            lastClicked();
             onClickOutside(e);
         }
     });
     return (react_1.default.createElement(react_1.default.Fragment, null,
-        react_1.default.createElement("div", { ref: wrapperRef }, children)));
+        react_1.default.createElement("div", { ref: wrapperRef, onClick: firstClicked }, children)));
 }
 exports.default = react_1.default.memo(OutsideWrapper);
 //# sourceMappingURL=index.js.map
--- dist/esm/components/index.js
+++ dist/esm/components/index.js
@@ -1,18 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useOutsideAlerter } from "../hooks";
 function OutsideWrapper(props) {
     // destructuring props
     var children = props.children, onClickOutside = props.onClickOutside;
     // use ref for detect element
     var wrapperRef = useRef(null);
+    var _a = useState(false), isClicked = _a[0], setIsClicked = _a[1];
+    var firstClicked = function () {
+        setIsClicked(true);
+    };
+    var lastClicked = function () {
+        setIsClicked(false);
+    };
     // use hooks outside and every change compare to current ref
     useOutsideAlerter(wrapperRef, function (e) {
-        if (onClickOutside) {
+        if (onClickOutside && isClicked) {
+            lastClicked();
             onClickOutside(e);
         }
     });
     return (React.createElement(React.Fragment, null,
-        React.createElement("div", { ref: wrapperRef }, children)));
+        React.createElement("div", { ref: wrapperRef, onClick: firstClicked }, children)));
 }
 export default React.memo(OutsideWrapper);
 //# sourceMappingURL=index.js.map
