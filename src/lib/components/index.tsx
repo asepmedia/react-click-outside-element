233b0cd14f9e693d0d97acca33e7167e1ba8436d
asepmedia <edukasimedia25@gmail.com>
2023-06-30 15:59:04 +0700

    feat: add classname

--- src/lib/components/index.tsx
+++ src/lib/components/index.tsx
@@ -5,43 +5,44 @@ import { useOutsideAlerter } from "../hooks";
  * Declare Interface for OutsideWrapper
  */
 interface IOutsideWrapperProps {
-    onClickOutside?: React.MouseEventHandler<MouseEvent>;
-    children: React.ReactNode;
+  children: React.ReactNode;
+  className?: string;
+  onClickOutside?: React.MouseEventHandler<MouseEvent>;
 }
 
 function OutsideWrapper(props: IOutsideWrapperProps) {
-    // destructuring props
-    const { children, onClickOutside } = props;
+  // destructuring props
+  const { children, onClickOutside } = props;
 
-    // use ref for detect element
-    const wrapperRef = useRef(null);
-    const [isClicked, setIsClicked] = useState(false);
+  // use ref for detect element
+  const wrapperRef = useRef(null);
+  const [isClicked, setIsClicked, className] = useState(false);
 
-    const firstClicked = () => {
-        setIsClicked(true);
-    };
-    const lastClicked = () => {
-        setIsClicked(false);
-    };
+  const firstClicked = () => {
+    setIsClicked(true);
+  };
+  const lastClicked = () => {
+    setIsClicked(false);
+  };
 
-    // use hooks outside and every change compare to current ref
-    useOutsideAlerter(
-        wrapperRef,
-        (e: React.MouseEvent<MouseEvent, MouseEvent>) => {
-            if (onClickOutside && isClicked) {
-                lastClicked();
-                onClickOutside(e);
-            }
-        }
-    );
+  // use hooks outside and every change compare to current ref
+  useOutsideAlerter(
+    wrapperRef,
+    (e: React.MouseEvent<MouseEvent, MouseEvent>) => {
+      if (onClickOutside && isClicked) {
+        lastClicked();
+        onClickOutside(e);
+      }
+    }
+  );
 
-    return (
-        <>
-            <div ref={wrapperRef} onClick={firstClicked}>
-                {children}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div ref={wrapperRef} className={className} onClick={firstClicked}>
+        {children}
+      </div>
+    </>
+  );
 }
 
 export default React.memo(OutsideWrapper);
